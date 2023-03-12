6cfd416683bcccd33e6b0a7c3eeb554d942fe6f8
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-03-12 15:58:07 +0300

    new task

---
+++ script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("myButt");
+
+button.addEventListener("mouseover", function() {
+  button.style.backgroundColor = "black";
+  button.style.color = "#fff";
+
+});
